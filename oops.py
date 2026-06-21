0f95926d84725a21843e98dfe952244727ffea85
hassanzaheer88 <sh.hassan607@gmail.com>
2026-06-22 03:40:10 +0500

    constructors

--- oops.py
+++ oops.py
@@ -23,10 +23,44 @@ class Facotory:             # class name should be CAPITAL
     
     def hello(self):
         print("Hello world")
-    print("Hello I am getting initialized")
+    #print("Hello I am getting initialized")
 
 # Facotory()      # without this , the above class can be initializes.
 # To access mehtods and attributes we use the Factory().
-print(Facotory().a)
-Facotory().hello()      # At first time all the things will be initialized. 
+# print(Facotory().a)
+# Facotory().hello()      # At first time all the things will be initialized. 
 #   so "Hello I am getting initialized" will be print only one time.
+
+# ------ Objects --------
+
+# obj = Facotory()        # obj = object , it has all the powers of class.
+# print(obj.a)
+# obj.hello()
+# obj2 = Facotory()
+# obj3 = Facotory()
+
+
+        
+# Constructors are used to accept parameters from the user.
+class Facotory:             
+    def __init__(self,material,zips,pockets):          # Parameters ask from the user when the class call happens.
+        # self keyword is used for targeting the location of obj1.
+        # print(self)             # Facotory object at 0x0000019D7E046A50>(obj1)
+                                # Facotory object at 0x0000019D7E2C8A50>(obj2)
+        self.material = material
+        self.zips = zips
+        self.pockets = pockets
+    
+    def show(self):
+        print(f"Your objects details are {self.material}, {self.zips},{self.pockets}")
+    
+        
+obj1 = Facotory("leather",3,2)
+obj2 = Facotory("nylon",3,3)
+# print(obj1.pockets)
+# print(obj2.pockets)
+
+obj1.show()
+obj2.show()
+
+
