7c780c0c9ae7d664c6b9075c01bfb92411bcbb95
hassanzaheer88 <sh.hassan607@gmail.com>
2026-06-22 03:54:20 +0500

    types of methods

--- oops.py
+++ oops.py
@@ -60,7 +60,33 @@ obj2 = Facotory("nylon",3,3)
 # print(obj1.pockets)
 # print(obj2.pockets)
 
-obj1.show()
-obj2.show()
+# obj1.show()
+# obj2.show()
+
+# -----------------------------------------------------------------------
+# 
+# --------- Types of Variables and Methods--------------
+
+class Animal:
+    name = "lion"       # class Attribute
+
+    def __init__(self,age):
+        self.age = age      # Instance Attribute
+    def show(self):         # self represents targeting the object, so it is instance method.
+        print("Hello this is self method")
+        
+    @classmethod
+    def hello(cls):         # cls targets the location of the class.
+        print("Hello this is class method")
+        
+    @staticmethod
+    def static():           # Static methode
+        print("It is static method")
+
+obj = Animal(12)
+# obj.show()
+# obj.hello()         # The above cls is not targeting this object, it is targeting the Animal class.
+# obj.static()
+# obj.show()              # without using above self keyword, the error will occur.
 
 
